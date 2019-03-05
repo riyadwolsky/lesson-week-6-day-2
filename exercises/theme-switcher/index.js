f7494abc426adabe2c9360f9196e2fcfffcc67bb
riyadh <riyadwolsky@gmail.com>
2019-03-05 18:38:12 +0300

    HW

--- exercises/theme-switcher/index.js
+++ exercises/theme-switcher/index.js
@@ -1,13 +1,38 @@
 // click event for gray button
-
+$("#grayButton").on("click",switchGray);
+$("#whiteButton").on("click",switchWhite);
 // click event for white button
 
-function switchGray() {
-    // change background to gray
-    // change font to white
-}
 
-function switchWhite() {
+//function switchGray() {
+//  document.body.style.background="gray";
+//  document.body.style.color="white";
+    // change background to gray
+
+//}
+
+//function switchWhite() {
+//  document.body.style.background="white";
+//  document.body.style.color="black";
     // change background to white
-    // change font to black
-}
+  //}
+
+
+  function switchGray() {
+  let body=$("body");
+  body.css({
+    "background":"gray",
+    "color":"white"
+  })
+
+
+  }
+  function switchWhite() {
+  let body=$("body");
+  body.css({
+    "background":"white",
+    "color":"black"
+  })
+
+
+  }
